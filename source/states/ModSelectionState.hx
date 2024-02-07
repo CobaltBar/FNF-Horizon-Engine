88802af5bceaff7c3add14baeb8038ee6df30b78
CobaltBar <79053181+CobaltBar@users.noreply.github.com>
2024-02-06 18:54:25 -0700

    bleh

--- source/states/ModSelectionState.hx
+++ source/states/ModSelectionState.hx
@@ -12,15 +12,29 @@ class ModSelectionState extends MusicMenuState
 
 	public override function create():Void
 	{
+		bpm = 0;
 		setupMenu();
 		createMenuBackdrop();
 		createMenuOptions(ModManager.discoveredMods);
+		var i:Int = 0;
+		new FlxTimer().start(0.1, (t:FlxTimer) ->
+		{
+			var trueX:Float = menuOptions[i].x;
+			menuOptions[i].x -= (2000 * (i % 2 == 0 ? 1 : -1));
+			menuOptions[i].visible = true;
+			FlxTween.tween(menuOptions[i], {x: trueX}, 1, {
+				type: ONESHOT,
+				ease: FlxEase.expoOut,
+			});
+			i++;
+		}, menuOptions.length);
 		super.create();
 	}
 
 	public override function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+
 		hue += elapsed * 15;
 		hue %= 359;
 		backdrop.color = FlxColor.fromHSB(hue, 1, 1);
@@ -30,16 +44,25 @@ class ModSelectionState extends MusicMenuState
 	{
 		for (i in 0...mods.length)
 		{
-			var option = new Alphabet(FlxG.width / 2, 200 + (i * 100), mods[i].name, true, CENTER, 1.2);
+			var option = new Alphabet(FlxG.width / 2, 400 + (i * 100), mods[i].name, true, CENTER, 1.2);
 			option.cameras = [optionsCam];
+			option.visible = false;
 			add(option);
+			menuOptions.push(option);
 		}
+
+		var option = new Alphabet(FlxG.width / 2, 400 + (menuOptions.length * 100), "Global Mods Only", true, CENTER, 1.2);
+		option.cameras = [optionsCam];
+		option.visible = false;
+		add(option);
+		menuOptions.push(option);
 	}
 
 	public function createMenuBackdrop():Void
 	{
-		backdrop = new FlxBackdrop(FlxGridOverlay.create(128, 128, 256, 256, true, 0xffffffff, 0xffcccccc).graphic);
-		backdrop.velocity.set(50, 30);
+		backdrop = new FlxBackdrop(Path.image("menuBGDesat"));
+		backdrop.scale.set(1.4, 1.4);
+		backdrop.velocity.set(50, 0);
 		backdrop.screenCenter();
 		backdrop.cameras = [menuCam];
 		add(backdrop);
