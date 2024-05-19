f7062ed8fdede446e0a355edd9ce3a2101815138
Cobalt Bar <79053181+CobaltBar@users.noreply.github.com>
2024-05-19 01:34:00 -0700

    git is lying to me

--- source/states/ErrorState.hx
+++ source/states/ErrorState.hx
@@ -7,6 +7,7 @@ class ErrorState extends MusicState
 {
 	var errorTitle:FlxText;
 	var errorDescription:FlxText;
+	var errorControls:FlxText;
 
 	public override function create():Void
 	{
@@ -14,16 +15,18 @@ class ErrorState extends MusicState
 		super.create();
 		DiscordRPC.change('In The Menus', 'Error Screen');
 		shouldBop = false;
-		add(errorTitle = Util.createText(FlxG.width * .5, -100, 'Error Caught', 36, Path.font('vcr'), 0xFFFF0000, CENTER));
+		add(errorTitle = Util.createText(FlxG.width * .5, Settings.data.reducedMotion ? 75 : -100, 'Error Caught', 36, Path.font('vcr'), 0xFFFF0000, CENTER));
 		errorTitle.screenCenter(X);
-		add(errorDescription = Util.createText(0, FlxG.height + 100, @:privateAccess Log.log, 18, Path.font('vcr'), 0xFFFFFFFF, LEFT));
-		add(Util.createText(0, FlxG.height - 50,
+		add(errorDescription = Util.createText(0, Settings.data.reducedMotion ? 175 : FlxG.height + 100, @:privateAccess Log.log, 18, Path.font('vcr'),
+			0xFFFFFFFF, LEFT));
+		add(errorControls = Util.createText(0, Settings.data.reducedMotion ? FlxG.height + 40 : FlxG.height + 100,
 			'Restart Engine: ${Settings.data.keybinds.get('accept')[0].toString()}/${Settings.data.keybinds.get('accept')[1].toString()}\nCreate Log and Restart: ${Settings.data.keybinds.get('back')[0].toString()}/${Settings.data.keybinds.get('back')[1].toString()}',
 			18, Path.font('vcr'), 0xFF00FF00, LEFT));
 		if (!Settings.data.reducedMotion)
 		{
 			FlxTween.tween(errorTitle, {y: 75}, 1, {type: ONESHOT, ease: FlxEase.expoOut});
 			FlxTween.tween(errorDescription, {y: 175}, 1, {type: ONESHOT, ease: FlxEase.expoOut});
+			FlxTween.tween(errorControls, {y: FlxG.height - 40}, 1, {type: ONESHOT, ease: FlxEase.expoOut});
 		}
 	}
 
