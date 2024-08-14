2953065a781dbc32c0b8838b8a115b2fd41e9f07
Cobalt Bar <79053181+CobaltBar@users.noreply.github.com>
2024-08-14 08:31:18 -0700

    i hope this fixes it

--- source/util/VersionMacro.hx
+++ source/util/VersionMacro.hx
@@ -4,16 +4,14 @@ import haxe.macro.Expr;
 
 class VersionMacro
 {
-	#if macro
+	// I think there might be a bug with this number increasing
+	#if (macro && !display_details && !display)
 	public static function run():Void
 	{
-		// ok wtf lime
-		#if (!display_details && !display)
 		if (sys.FileSystem.exists('.build'))
 			sys.io.File.saveContent('.build', '${Std.parseInt(sys.io.File.getContent('.build')) + 1}');
 		else
 			sys.io.File.saveContent('.build', '0');
-		#end
 	}
 	#end
 
